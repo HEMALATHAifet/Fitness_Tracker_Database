3c85cfc06254e2fe4c848f1d9859e7eb0f99fbb9
A.HEMALATHA <95863646+HEMALATHAifet@users.noreply.github.com>
2025-10-28 21:07:47 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO Users VALUES
+(1, 'Aarav', 25, 'Male'),
+(2, 'Isha', 28, 'Female'),
+(3, 'Rohan', 22, 'Male'),
+(4, 'Diya', 30, 'Female');
+
+INSERT INTO Workouts VALUES
+(101, 1, 'Running', 40, 350, '2025-10-20'),
+(102, 1, 'Yoga', 60, 200, '2025-10-22'),
+(103, 2, 'Cycling', 45, 400, '2025-10-21'),
+(104, 2, 'Running', 30, 280, '2025-10-22'),
+(105, 3, 'Gym', 50, 500, '2025-10-23'),
+(106, 4, 'Walking', 60, 250, '2025-10-22'),
+(107, 4, 'Swimming', 45, 450, '2025-10-23');
+
+INSERT INTO Goals VALUES
+(201, 1, 'Burn 2000 calories', 2000, '2025-10-31'),
+(202, 2, 'Run 10 sessions', 10, '2025-11-15'),
+(203, 3, 'Lose 3 kg', 3, '2025-11-10'),
+(204, 4, 'Workout 5 days', 5, '2025-10-31');
